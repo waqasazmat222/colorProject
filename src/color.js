8e0f10527648727d990868cd1d78a3700d45fd92
Waqas Azmat <waqasazmat222@gmail.com>
2021-04-10 04:00:32 +1000

    first commit

---
+++ src/color.js
@@ -0,0 +1,11 @@
+
+
+const Color=()=>{
+
+return<>
+      <div style={{margin:"0 auto", width:"300px",marginTop:"200px"}}>
+        <img src="http://localhost:9000/color.png" />  
+      </div>
+</>
+}
+export default Color;
